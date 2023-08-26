bebc5e4bed1622ef3c89be45fe7330e64bad0195
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-08-26 03:44:32 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+import random
+
+print("racist roullete")
+
+print("play")
+cowardornot = input("")
+slots =()
+bullet = "n", "b", "w", "r",
+
+slots = random.choice(bullet)
+
+fire = "igger"
+
+if cowardornot == ("y"):
+
+    print(slots+fire)
+
+else:
+
+    print("pussy")
